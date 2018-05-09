36b8477191155dc770db6c525ce841242756482b
Fernando Kulaif <fkulaif@users.noreply.github.com>
2018-05-08 23:55:50 -0300

    inserting some html

--- login.php
+++ login.php
@@ -1,4 +1,17 @@
+<!-- http://localhost/.../.../login.php  -->
+
+<form action="the_post.php" method="post">
+   <input type="text" name="name">
+   <input type="submit">  
+</form>
+
+
+</body>
+</html>
+
+<!-- ?php echo $_POST['name']; ?-->
 <?php
+//<?php echo $_POST['name']; ?>
 
    if(isset($_POST ['submit'])) {
       //echo "yes we got here";
